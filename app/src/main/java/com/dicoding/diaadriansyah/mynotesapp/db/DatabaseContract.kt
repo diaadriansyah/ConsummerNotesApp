4e846d6a1f9b615393317de7929df1ad9f1735d1
diaadriansyah <diaadriansyah4@gmail.com>
2020-11-21 12:48:01 +0700

    Menambahkan Authority

--- app/src/main/java/com/dicoding/diaadriansyah/mynotesapp/db/DatabaseContract.kt
+++ app/src/main/java/com/dicoding/diaadriansyah/mynotesapp/db/DatabaseContract.kt
@@ -1,9 +1,16 @@
 package com.dicoding.diaadriansyah.mynotesapp.db
 
+import android.net.Uri
 import android.provider.BaseColumns
 
-internal class DatabaseContract {
-
+object DatabaseContract {
+    // Authority yang digunakan
+    const val AUTHORITY = "com.dicoding.diaadriansyah.mynotesapp"
+    const val SCHEME = "content"
+    /*
+   Penggunaan Base Columns akan memudahkan dalam penggunaan suatu table
+   Untuk id yang autoincrement sudah default ada di dalam kelas BaseColumns dengan nama field _ID
+    */
     internal class NoteColumns : BaseColumns {
         companion object {
             const val TABLE_NAME = "note"
@@ -11,6 +18,11 @@ internal class DatabaseContract {
             const val TITLE = "title"
             const val DESCRIPTION = "description"
             const val DATE = "date"
+            // Base content yang digunakan untuk akses content provider
+            val CONTENT_URI: Uri = Uri.Builder().scheme(SCHEME)
+                    .authority(AUTHORITY)
+                    .appendPath(TABLE_NAME)
+                    .build()
         }
     }
 }
